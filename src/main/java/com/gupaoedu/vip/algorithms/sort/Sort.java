597e3ba3a6404d87830f944a65747270f450da79
2358723997 <2358723997@qq.com>
2021-04-15 09:18:47 +0800

    modify

---
+++ src/main/java/com/gupaoedu/vip/algorithms/sort/Sort.java
@@ -0,0 +1,15 @@
+package com.gupaoedu.vip.algorithms.sort;
+
+/**
+ * Sort类
+ *
+ * @author wangjixue
+ * @date 4/15/21 8:59 AM
+ */
+public class Sort {
+    public static void main(String[] args) {
+        //冒泡排序
+        //插入排序
+        //选择排序
+    }
+}
